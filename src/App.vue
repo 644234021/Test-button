d9b7418369eb69f3a54957083a0132dc10bdbe3f
Perapat <644234021@parichat.skru.ac.th>
2024-02-02 18:42:35 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+  import {useCounterStore} from './stores/counter'
+  const store = useCounterStore()
+
+  function indrease(){
+    store.increment()
+  }
+
+  function decrease(){
+    store.decrement()
+  }
+</script>
+
+<template>
+  <h1 class="d-flex align-items-center justify-content-center"><b>Count :</b> {{ store.count }}</h1>
+  <h1 class="d-flex align-items-center justify-content-center"><b>Double Count :</b> {{ store.doubleCount }}</h1>
+  <div class="mt-5 d-grid gap-2 d-flex align-items-center justify-content-center">
+    <button type="button" class="btn btn-success btn-lg" @click="indrease()">+ Indrease</button>
+    <button type="button" class="btn btn-danger btn-lg" @click="decrease()">- Decrease</button>
+  </div>
+
+  
+  <div>
+  <button type="button" class="btn btn-success btn-lg" @click="indrease()">+ IndreaseX</button>
+  <button type="button" class="btn btn-danger btn-lg" @click="decrease()">- DecreaseX</button>
+  <button type="button" class="btn btn-info btn-lg">Info</button>
+  </div>
+
+  
+ 
+</template>
+
+<style scoped>
+
+</style>
+
